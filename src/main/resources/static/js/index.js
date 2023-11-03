e93aa4201474a69c3069b00337d09c3842ef2b5d
windor <windor@vip.qq.com>
2023-11-03 09:17:12 +0800

    更新UI

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -72,14 +72,37 @@ function getCheckedHwndSerialize() {
 
 // 多选框的选择
 $(function () {
+    $(".time-ms").each(function (i, obj) {
+        let ms = $(obj).html();
+        ms = Math.floor(ms / 1000);
+        let hour = Math.floor(ms / 3600);
+        if(hour < 10) {
+            hour = "0" + hour;
+        }
+        let minute = Math.floor(ms % 3600 / 60);
+        if(minute < 10) {
+            minute = "0" + minute;
+        }
+        let second = ms % 60;
+        if(second < 10) {
+            second = "0" + second;
+        }
+        $(obj).html(hour + ":" + minute + ":" + second)
+    })
     // 取消继承
     $("#ddt tr td:first-child input,th:first-child input").each(function (i, input) {
         $(input).click(function (e) {
             e.stopPropagation();
         });
     });
+    $("#ddt a").each(function (i, a) {
+        $(a).click(function (e) {
+            e.stopPropagation();
+        })
+    });
+
     // 点击input的上一级的时候就点击input
-    $("#ddt tr td:nth-child(-n+3),th:nth-child(-n+3)").click(function () {
+    $("#ddt tr td, th").click(function () {
         $(this).parent().find("input[type='checkbox']").click();
     })
     // 全选按钮
@@ -94,7 +117,7 @@ $(function () {
     $("#ddt-suspend").click(function () {
         let hwnds = getCheckedHwndSerialize();
         if(hwnds === "") {
-            windorAlert('提示', '请选中一些脚本后再执行本方法')
+            windorAlert('提示', '请 <b>选中一些脚本后</b> 再执行本方法')
         }else{
             postAjax('/util/suspend', hwnds, function (data) {
                 windorAlert('提示', '已尝试暂停选中的脚本');
@@ -104,7 +127,7 @@ $(function () {
     $("#ddt-continue").click(function () {
         let hwnds = getCheckedHwndSerialize();
         if(hwnds === "") {
-            windorAlert('提示', '请选中一些脚本后再执行本方法')
+            windorAlert('提示', '请 <b>选中一些脚本后</b> 再执行本方法')
         }else{
             postAjax('/util/continue', hwnds, function (data) {
                 windorAlert('提示', '已尝试恢复选中的脚本');
@@ -114,7 +137,7 @@ $(function () {
     $("#ddt-stop").click(function () {
         let hwnds = getCheckedHwndSerialize();
         if(hwnds === "") {
-            windorAlert('提示', '请选中一些脚本后再执行本方法')
+            windorAlert('提示', '请 <b>选中一些脚本后</b> 再执行本方法')
         }else{
             postAjax('/util/stop', hwnds, function (data) {
                 windorAlert('提示', '已尝试停止选中的脚本');
@@ -124,7 +147,7 @@ $(function () {
     $("#ddt-restart").click(function () {
         let hwnds = getCheckedHwndSerialize();
         if(hwnds === "") {
-            windorAlert('提示', '请选中一些脚本后再执行本方法')
+            windorAlert('提示', '请 <b>选中一些脚本后</b> 再执行本方法')
         }else{
             postAjax('/util/restart', hwnds, function (data) {
                 windorAlert('提示', '已尝试重启指定脚本');
@@ -134,7 +157,7 @@ $(function () {
     $("#ddt-remove").click(function () {
         let hwnds = getCheckedHwndSerialize();
         if(hwnds === "") {
-            windorAlert('提示', '请选中一些脚本后再执行本方法')
+            windorAlert('提示', '请 <b>选中一些脚本后</b> 再执行本方法')
         }else{
             postAjax('/util/remove', hwnds, function (data) {
                 windorAlert('提示', '已尝试移除指定脚本');
