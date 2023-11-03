d064c194b2490559d684d57a46a1a50ffd796e9b
windor <windor@vip.qq.com>
2023-11-03 09:23:07 +0800

    修改角度调整为可中断

--- src/main/java/cn/windor/ddtank/handler/impl/SimpleDDTankAngleAdjustMoveHandlerHandler.java
+++ src/main/java/cn/windor/ddtank/handler/impl/SimpleDDTankAngleAdjustMoveHandlerHandler.java
@@ -16,21 +16,21 @@ public class SimpleDDTankAngleAdjustMoveHandlerHandler implements DDTankAngleAdj
 
     @Override
     public boolean move(TowardEnum targetToward, int targetAngle, int triedTimes) {
-//        if (targetToward == TowardEnum.LEFT) {
-//            for (int i = 0; i < 3; i++) {
-//                keyboard.keyDown('a');
-//                delay(50);
-//                keyboard.keyUp('a');
-//                delay(100);
-//            }
-//        } else if (targetToward == TowardEnum.RIGHT) {
-//            for (int i = 0; i < 3; i++) {
-//                keyboard.keyDown('d');
-//                delay(50);
-//                keyboard.keyUp('d');
-//                delay(100);
-//            }
-//        }
+        if (targetToward == TowardEnum.LEFT) {
+            for (int i = 0; i < 3; i++) {
+                keyboard.keyDown('a');
+                delay(50, true);
+                keyboard.keyUp('a');
+                delay(100, true);
+            }
+        } else if (targetToward == TowardEnum.RIGHT) {
+            for (int i = 0; i < 3; i++) {
+                keyboard.keyDown('d');
+                delay(50, true);
+                keyboard.keyUp('d');
+                delay(100, true);
+            }
+        }
         return triedTimes <= 3;
     }
 }
